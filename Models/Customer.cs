d5ab8974f68a5090c7310a8bcb87f08b86650beb
Lluminatarion-8431 <Captain.a.8431@gmail.com>
2020-09-17 14:14:49 -0700

    Added display names to Customer model.

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -12,16 +12,26 @@ namespace TrashCollector.Models
     {
         [Key]
         public int CustomerId { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
         public string Street { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public string ZipCode { get; set; }
-        public string TrashDay { get; set; }
-        public string ExtraPickUpRequest { get; set; }
+        [Display(Name = "Zip Code")]
+        public int ZipCode { get; set; }
+        [Display(Name = "Pickup day (required)")]
+        public string TrashPickUpDay { get; set; }
+        [Display(Name = "One-time pickup date (optional) (i.e. MM/DD/YYYY)")]
+        public int ExtraPickUpRequest { get; set; }
+        [Display(Name = "Due balance on account")]
         public double AccountBalance { get; set; }
-        public string TemporaryPickUpSuspension { get; set; }
+        [Display(Name = "Pickup suspension start date (optional)")]
+        public string StartPickUpSuspension { get; set; }
+        [Display(Name = "Pickup suspension end date (optional)")]
+        public string EndPickUpSuspension { get; set; }
 
         [ForeignKey("IdentityUser")]
         public string IdentityUserId { get; set; }
